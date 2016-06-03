d3a3995d23dc2c3d6fffb600cde8c96a0f86abfd
Peter Andrews <Peter.Andrews@rsginc.com>
2016-06-03 15:53:42 -0400

    initial checkin. All files from \\WRJMDLPPW01\E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs and appear to have been made 2016-04-03/04

---
+++ TransCAD_outputs/deletefiles.bat
@@ -0,0 +1,22 @@
+REM temp transit skim files
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_WalkLocal.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_WalkBrt.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_WalkExpBus.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_WalkUrbRail.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_WalkComRail.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_DriveLocal.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_DriveBrt.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_DriveExpBus.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_DriveUrbRail.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\??_DriveComRail.mtx
+REM temp files from path building
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\*.tps
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\*_pnr_time.mtx
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\*_pnr_node.mtx
+REM temp files from mode choice post processing
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\???Temp.mtx
+REM temp files from the preliminary transit assignment for calculating the travel times
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\*PreloadFlow.bin
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\*PreloadFlow.dcb
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\*PreloadWalkFlow.bin
+del E:\Projects\Clients\Nashville\RunModel\CleanABMsetup\ToNashvilleMPO\2010\outputs\*PreloadWalkFlow.dcb
