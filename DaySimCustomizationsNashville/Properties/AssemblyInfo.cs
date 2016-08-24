6cf9fe04e7920568fdeee0e6c026c63041789088
Peter Andrews <Peter.Andrews@rsginc.com>
2016-08-24 12:49:53 -0400

    Add git version info

--- DaySimCustomizationsNashville/Properties/AssemblyInfo.cs
+++ DaySimCustomizationsNashville/Properties/AssemblyInfo.cs
@@ -22,15 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3d0bf5c8-440a-417e-8856-fc7373f4d488")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// All version information will be injected by GitVersion Task http://gitversion.readthedocs.io/
