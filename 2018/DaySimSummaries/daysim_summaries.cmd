07b364746213347f95ff71f4436ad1c79257dc17
Nagendra <nsdhakar@gmail.com>
2020-06-11 15:04:07 -0400

    added software folder for daysim summaries and provided relative r path in the run batch file.

--- 2018/DaySimSummaries/daysim_summaries.cmd
+++ 2018/DaySimSummaries/daysim_summaries.cmd
@@ -1,1 +1,1 @@
-"E:\Projects\Clients\Nashville\Model\BaseYear\Calibration\ABM_TCAD8_TAZSPLIT\2018\DaySimSummaries\software\R\R-3.4.4\bin\x64\R.exe" CMD BATCH --no-save main.R log.txt
+".\software\R\R-3.4.4\bin\x64\R.exe" CMD BATCH --no-save main.R log.txt
